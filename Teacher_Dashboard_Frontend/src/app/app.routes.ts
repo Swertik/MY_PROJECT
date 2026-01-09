4009a0d4d789e94fb2a1a0cc86d829047151dfb8
Swertik <herobrin664@gmail.com>
2026-01-09 18:53:18 +0300

    Настроил роутинг

--- Teacher_Dashboard_Frontend/src/app/app.routes.ts
+++ Teacher_Dashboard_Frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Auth } from './auth/auth';
+import { Dashboard } from './dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // По умолчанию идем на логин
+  { path: 'login', component: Auth },
+  { path: 'dashboard', component: Dashboard },
+];
