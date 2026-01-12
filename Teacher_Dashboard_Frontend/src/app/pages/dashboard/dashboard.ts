074fa85ea65abdb88d5d33f190d0986aa3a00036
Swertik <herobrin664@gmail.com>
2026-01-12 15:49:47 +0300

    Добавил управление с клавиатуры

--- Teacher_Dashboard_Frontend/src/app/pages/dashboard/dashboard.ts
+++ Teacher_Dashboard_Frontend/src/app/pages/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, ElementRef, HostListener, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { StudentDashboardItem } from '../../models/dashboard.model';
 import { Llmservice } from '../../services/llmservice';
 import { FormsModule } from '@angular/forms';
@@ -13,12 +13,26 @@ export class Dashboard implements OnInit {
   private raw_data = signal<StudentDashboardItem[]>([]);
 
   currentInput = signal("");
-
   searchChips = signal<string[]>([]);
+
+  isDropdownOpen = signal(false);
+  focusedIndex = signal(-1);
 
   lmsService = inject(Llmservice)
 
-  isDropdownOpen = signal(false);
+  @ViewChild('searchInput') searchInputRef!: ElementRef<HTMLInputElement>;
+
+  @HostListener('window:keydown', ['$event'])
+  handleGlobalKeyboardEvent(event: KeyboardEvent) {
+
+    const activeElement = document.activeElement as HTMLElement;
+    const isTypingSomewhere = activeElement.tagName === 'INPUT' || activeElement.tagName === 'TEXTAREA';
+    
+    if (!isTypingSomewhere && (event.key === 'Enter' || event.key === '/')) {
+      event.preventDefault();
+      this.searchInputRef.nativeElement.focus();
+    }
+  }
 
   allAvailableOptions = computed(() => {
     const students = this.raw_data();
@@ -61,6 +75,7 @@ export class Dashboard implements OnInit {
   selectOption(option: string) {
     this.searchChips.update(chips => [...chips, option]);
     this.currentInput.set('');
+    this.focusedIndex.set(-1);
   }
 
   removeChip(index: number) {
@@ -69,6 +84,7 @@ export class Dashboard implements OnInit {
 
   onInputFocus() {
     this.isDropdownOpen.set(true);
+    this.focusedIndex.set(0);
   }
 
   onInputBlur() {
@@ -87,5 +103,52 @@ export class Dashboard implements OnInit {
       error: (err) => console.error(err)
     });
   }
+
+  scrollToOption(index: number) {
+    const list = document.querySelector('.dropdown-list');
+    const item = list?.children[index] as HTMLElement;
+    if (item && list) {
+      item.scrollIntoView({ block: 'nearest' }); 
+    }
+  }
+
+  onKeyDown (event: KeyboardEvent){
+    console.log(1);
+    if (!this.isDropdownOpen() || this.dropdownOptions().length == 0) return;
+
+    const options = this.dropdownOptions();
+    const currentIndex = this.focusedIndex();
+
+    switch (event.key){
+      case "ArrowDown":
+      
+        event.preventDefault();
+
+        const nextIndex = (currentIndex + 1) % options.length;
+        this.focusedIndex.set(nextIndex);
+        this.scrollToOption(nextIndex);
+        break;
+      case "ArrowUp":
+        event.preventDefault();
+
+        const prevIndex = (currentIndex - 1 + options.length) % options.length;
+        this.focusedIndex.set(prevIndex);
+        this.scrollToOption(prevIndex);
+        break;
+      case "Enter":
+        event.preventDefault();
+
+        if (currentIndex >= 0 && currentIndex < options.length){
+          this.selectOption(options[currentIndex])
+        }
+        break;
+      case "Escape":
+        this.isDropdownOpen.set(false);
+        this.searchInputRef.nativeElement.blur();
+        break;
+    }
+  }
+
+
   
 }
